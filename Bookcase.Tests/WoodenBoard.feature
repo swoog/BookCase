4d20576663c6d7d48e84b0eeadf04b758aa739d8
Aurélien GALTIER <aurelien.galtier@live.fr>
2014-11-13 10:45:44 +0100

    Add woode board definition

---
+++ Bookcase.Tests/WoodenBoard.feature
@@ -0,0 +1,22 @@
+﻿Feature: Wooden board
+	In order to avoid a bookcase
+	As a user
+	I want to define a wooden board
+
+Scenario: A wooden board
+	Given a wooden board of "400x300x15" mm
+	Then the width is 300 mm
+	And the height is 400 mm
+	And the thickness is 15 mm
+
+Scenario: Another wooden board
+	Given a wooden board of "300x400x15" mm
+	Then the width is 300 mm
+	And the height is 400 mm
+	And the thickness is 15 mm
+
+Scenario: A wooden board with the thickness in first
+	Given a wooden board of "15x300x400" mm
+	Then the width is 300 mm
+	And the height is 400 mm
+	And the thickness is 15 mm
---
+++ Bookcase.Tests/WoodenBoardSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Bookcase.Tests
+{
+    [Binding]
+    public class WoodenBoardSteps
+    {
+        [Given(@"a wooden board of ""(.*)"" mm")]
+        public void GivenAWoodenBoardOfMm(string woodenSize)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the width is (.*) mm")]
+        public void ThenTheWidthIsMm(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the height is (.*) mm")]
+        public void ThenTheHeightIsMm(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the thickness is (.*) mm")]
+        public void ThenTheThicknessIsMm(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
